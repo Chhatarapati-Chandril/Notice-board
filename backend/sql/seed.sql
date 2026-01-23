a08729b64bcb6911e3bcdc3bd12cfec9192987a0
Chhatarapati Chandril <chandril6a@gmail.com>
2026-01-24 00:13:24 +0530

    created and initialized tables

---
+++ backend/sql/seed.sql
@@ -0,0 +1,19 @@
+show databases;
+use notice_board;
+select database();
+show tables;
+
+
+INSERT IGNORE INTO notice_categories (name) VALUES
+('Examination Cell'),
+('Training and Placement Cell'),
+('Academics'),
+('Student Council'),
+('NSS'),
+('Impact Club'),
+('Photography Club'),
+('Technical Society'),
+('Sports Club'),
+('General');
+
+
