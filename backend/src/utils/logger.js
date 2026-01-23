fb2dfa73f2ccb35d889ca612908ba980f02967b9
Chhatarapati Chandril <chandril6a@gmail.com>
2026-01-24 00:12:24 +0530

    created utils

---
+++ backend/src/utils/logger.js
@@ -0,0 +1,13 @@
+const isDev = process.env.NODE_ENV === "development";
+
+export const devLog = (...args) => {
+  if (isDev) {
+    console.log(...args);
+  }
+};
+
+export const devError = (...args) => {
+  if (isDev) {
+    console.error(...args);
+  }
+};
