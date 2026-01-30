1ade720e041846be80e7ffda3f6d733992c0f255
Chhatarapati Chandril <chandril6a@gmail.com>
2026-01-30 10:47:35 +0530

    Refactor app.js for consistent formatting and improved readability; add missing semicolons

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,29 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import { JSON_LIMIT } from "./constants.js"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { JSON_LIMIT } from "./constants.js";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  }),
+);
 
-app.use(express.json({limit: JSON_LIMIT}))
-app.use(express.urlencoded({extended: true, limit: JSON_LIMIT}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
+app.use(express.json({ limit: JSON_LIMIT }));
+app.use(express.urlencoded({ extended: true, limit: JSON_LIMIT }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // routes import
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
 import noticeRoutes from "./routes/notice.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 
 app.use("/api/v1/noticeboard/notices", noticeRoutes);
 app.use("/api/v1/noticeboard/categories", categoryRoutes);
-app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/auth", authRoutes);
 
-export default app
+export default app;
