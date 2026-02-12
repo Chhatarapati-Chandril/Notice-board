a24a3fc376e4d3d8ac5e92d2d241e37b566fbbb2
Chhatarapati Chandril <chandril6a@gmail.com>
2026-02-12 19:34:09 +0530

    changed email validation

--- backend/src/validators/email.rule.js
+++ backend/src/validators/email.rule.js
@@ -2,7 +2,7 @@ const normalizeEmail = (email) => {
   if (typeof email !== "string") return null;
 
   const normalized = email.toLowerCase().trim();
-  const regex = /^[a-z0-9]+@iiitsonepat\.ac\.in$/;
+  const regex = /^[a-z0-9.]+@iiitsonepat\.ac\.in$/;
 
   return regex.test(normalized) ? normalized : null;
 };
