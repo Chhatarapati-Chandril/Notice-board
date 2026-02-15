35464642f8f7bc096e08ac48d4756eadb49ae4ba
Chhatarapati Chandril <chandril6a@gmail.com>
2026-02-15 23:31:44 +0530

    feat: integrate Brevo for email sending and remove Nodemailer configuration

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,28 +1,48 @@
-import nodemailer from "nodemailer";
+import Brevo from "@getbrevo/brevo";
 import ApiError from "./ApiError.js";
 import { devError, devLog } from "./logger.js";
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false, // must be false for 587
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: Number(process.env.EMAIL_PORT),
+//   secure: false, // must be false for 587
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+const apiInstance = new Brevo.TransactionalEmailsApi();
+
+apiInstance.setApiKey(
+  Brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
+
 
 export const sendEmail = async ({ to, subject, text, html }) => {
   try {
-    await transporter.sendMail({
-      from: `"Notice Board" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html,
-    });
-  } catch (error) {
-    devError("EMAIL ERROR:", error);
+    const sendSmtpEmail = new Brevo.SendSmtpEmail();
+
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = html || `<p>${text}</p>`;
+    sendSmtpEmail.sender = {
+      name: "Notice Board",
+      email: process.env.EMAIL_FROM,
+    };
+    sendSmtpEmail.to = [{ email: to }];
+
+    const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+    devLog("BREVO SUCCESS:", response.body.messageId);
+
+    return response;
+  } 
+  catch (error) {
+    devError("BREVO ERROR:", error);
     throw new ApiError(500, error.message);
   }
 };
+
